58e707d3a9f6d15217ccd12c46ba48d88023104f
rmtsukuru <randolph.113@osu.edu>
2013-09-18 02:01:55 -0400

    Initial commit - viperfish splash page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bioluminous::Application.config.secret_key_base = '8f093e315661a0aa680c091e610618ef6aeeec51cb1455adfd0300c3a9a5736fadd8ab0dd7ffb40b8865ba7151423b9fc0d88198bf4eee0947e113e8919c937c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bioluminous::Application.config.session_store :cookie_store, key: '_bioluminous_session'
